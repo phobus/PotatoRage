277a01f8999cb166aafa3c45195115397ba0890c
phobus <immalloc@gmail.com>
2015-04-14 21:42:23 +0200

    provider newpct1

---
+++ potatorage/providers/newpct1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+http://www.newpct1.com/descarga-torrent/pelicula/%s
+http://www.newpct1.com/descarga-torrent/serie/%s
+
+http://tumejorjuego.com/download/index.php?link=%s
+
+https://github.com/skasi7/htpc/blob/master/flexget/urlrewrite_newpct.py
+"""
